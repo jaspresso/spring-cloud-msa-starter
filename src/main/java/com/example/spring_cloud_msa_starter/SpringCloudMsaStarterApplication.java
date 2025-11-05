37e6f6ea3c3fc2c9b199870561826e65cfed51d5
jaspresso <jaspresso@gmail.com>
2025-11-05 10:27:43 +0900

    ch02: 프로젝트 생성 및 기본모듈 셋팅

---
+++ src/main/java/com/example/spring_cloud_msa_starter/SpringCloudMsaStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cloud_msa_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMsaStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMsaStarterApplication.class, args);
+	}
+
+}
